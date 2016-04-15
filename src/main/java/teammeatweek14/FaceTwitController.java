1b36efa9e26d60a2ee300c7e694e18e3e0263254
Kevin Ramdass <ramdass.kevin@gmail.com>
2016-04-15 15:26:05 -0700

    Added LinkedIn service
    After connection, it does not redirect to the home page.
    spring.social.auto-connection-views=true is needed for linkedin. We can
    look into this further

--- src/main/java/teammeatweek14/FaceTwitController.java
+++ src/main/java/teammeatweek14/FaceTwitController.java
@@ -1,5 +1,7 @@
 package teammeatweek14;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.social.connect.ConnectionRepository;
@@ -7,6 +9,8 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.linkedin.api.LinkedInProfile;
 import org.springframework.social.twitter.api.CursoredList;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.TwitterProfile;
@@ -20,12 +24,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class FaceTwitController {
 	private Facebook facebook;
 	private Twitter twitter;
+	private LinkedIn linkedin;
 	private ConnectionRepository connectionRepository;
 
     @Inject
-    public FaceTwitController(Facebook facebook, Twitter twitter, ConnectionRepository connectionRepository) {
+    public FaceTwitController(Facebook facebook, Twitter twitter, LinkedIn linkedin, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
         this.twitter = twitter;
+        this.linkedin = linkedin;
 		this.connectionRepository = connectionRepository;
     }
 
@@ -41,6 +47,10 @@ public class FaceTwitController {
             model.addAttribute(twitter.userOperations().getUserProfile());
             CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
             model.addAttribute("friends", friends);
+        }
+        
+        if (connectionRepository.findPrimaryConnection(LinkedIn.class) != null) {
+        	 model.addAttribute("linkedinProfile", linkedin.profileOperations().getUserProfile());
         }
 
         return "homePage";
