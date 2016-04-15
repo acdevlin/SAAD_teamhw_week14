9f6e4898acf3b635bc803714a6c76763a09fd982
Baby's <acdevlin42@gmail.com>
2016-04-14 20:55:31 -0700

    Integrated Twitter. Sloppy at this stage, but works as proof-of-concept

--- src/main/java/teammeatweek14/FaceTwitController.java
+++ src/main/java/teammeatweek14/FaceTwitController.java
@@ -6,6 +6,9 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.TwitterProfile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +16,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
-
-    private Facebook facebook;
+public class FaceTwitController {
+	private Facebook facebook;
+	private Twitter twitter;
 	private ConnectionRepository connectionRepository;
 
     @Inject
-    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+    public FaceTwitController(Facebook facebook, Twitter twitter, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
+        this.twitter = twitter;
 		this.connectionRepository = connectionRepository;
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public String helloFacebook(Model model) {
+    public String helloFaceTwit(Model model) {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
+        }
+        if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
+        	return "redirect:/connect/twitter";
         }
 
         model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
         PagedList<Post> feed = facebook.feedOperations().getFeed();
         model.addAttribute("feed", feed);
+        
+        model.addAttribute(twitter.userOperations().getUserProfile());
+        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
+        model.addAttribute("friends", friends);
+        
         return "hello";
     }
-
 }
